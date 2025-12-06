ec025cd224d70039dd22f2a5a4bc6d7a106bfdbd
Yahya Ismial <yahya.ismail06@eng-st.cu.edu.eg>
2025-12-06 22:48:35 +0200

    patient final

--- src/hospitalSystem/Patient.java
+++ src/hospitalSystem/Patient.java
@@ -15,4 +15,12 @@ public class Patient extends Person {
     public int getMedicalHistoryID(){
         return medicalHistoryID;
     }
+    @Override
+    public void display(){
+        System.out.println("Name: " + getname());
+        System.out.println("ID: " + getID());
+        System.out.println("Email: " + getEmail());
+        System.out.println("Phone Number: " + getPhoneNumber());
+        System.out.println("medicalHistoryID: " + this.medicalHistoryID);
+    }
 }
